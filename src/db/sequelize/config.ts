a8c3ac28ca86cec358ed436fabd86c1007304413
DavidFune <david.funy28@gmail.com>
2023-02-26 23:39:04 -0300

    main: confo webpack and template

--- src/db/sequelize/config.ts
+++ src/db/sequelize/config.ts
@@ -1,4 +1,4 @@
-import {config as readEnv} from "dotenv"
+import { config as readEnv } from "dotenv"
 import { join } from "path";
 
 type Config = {
@@ -13,18 +13,36 @@ type Config = {
 }
 
 function makeConfig(envFile): Config {
-    const output = readEnv({path: envFile})
-    return{
-        db: {
-            vendor: output.parsed.DB_VENDOR as any,
-            password: output.parsed.PASSWORD as any,
-            host: output.parsed.DB_HOST,
-            port: 5441,
-            user: output.parsed.USER,
-            logging: output.parsed.DB_LOGGING === "true",   
+    const output = readEnv({ path: envFile })
+
+    console.log('ENV:', process.env);
+    
+    if (process.env?.STAGE_NAME == 'Dev') {
+        return {
+            db: {
+                vendor: process.env.DB_VENDOR as any,
+                password: process.env.PASSWORD as any,
+                host: process.env.DB_HOST,
+                port: 5441,
+                user: process.env.USER,
+                logging: process.env.DB_LOGGING === "true",
+            }
+        }
+
+    } else {
+        return {
+            db: {
+                vendor: output.parsed.DB_VENDOR as any,
+                password: output.parsed.PASSWORD as any,
+                host: output.parsed.DB_HOST,
+                port: 5441,
+                user: output.parsed.USER,
+                logging: output.parsed.DB_LOGGING === "true",
+            }
         }
     }
 }
+
 
 const envTestingFile = join(__dirname, "../../../.env")
 export const configDB = makeConfig(envTestingFile)
---
+++ src/handlers/product/product.spec.ts
@@ -0,0 +1,41 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { get_by_id_product, post_product } from './product';
+const res = require("../../../events/event.json");
+
+describe('Product test the http methodos', () => {
+    
+    let event: APIGatewayProxyEvent;
+    const product_values = {
+        name: 'Mouse-Usb-03',
+        price: 50.00,
+        description: "Mouse para jogos"
+    }
+
+    beforeEach( async () => {
+        event = res
+    })
+
+    it('should post product', async () => {
+
+        event = {
+            ...event,
+            httpMethod: "POST",
+            body: JSON.stringify({product: product_values})
+        }
+
+        const result = await post_product(event);
+        expect(result.statusCode).toEqual(200);
+    });
+
+    it('should get by id product', async () => {
+
+        const event = { 
+            httpMethod: 'GET', 
+            pathParameters: { 
+                product_id: '18' 
+            } 
+        } 
+        const result = await get_by_id_product(event as any);
+        expect(result.statusCode).toEqual(200);
+    });
+});
