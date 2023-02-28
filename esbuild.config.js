5b5f5286cb2ef5fa2a3df8c7d1785a7006bfc491
DavidFune <david.funy28@gmail.com>
2023-02-28 19:16:07 -0300

    esbuild: basic config

---
+++ esbuild.config.js
@@ -0,0 +1,19 @@
+const esbuild = require('esbuild')
+const esbuildPluginTsc = require('esbuild-plugin-tsc');
+const glob = require("tiny-glob");
+( async () => {
+    let entryPoints = await glob("./src/*.ts");
+    esbuild.build({
+        entryPoints,
+        outdir: 'dist',
+        bundle: true,
+        platform: 'node',
+        sourcemap: true,
+        target: 'node14',
+        plugins: [
+            esbuildPluginTsc({
+                tsconfigPath: './tsconfig.json',
+            }),
+        ],
+    }).catch(() => process.exit(1));
+})();
